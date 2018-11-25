1fbe47de88bf04a9c63cc73a85f457914ce0afe2
nghiemdao <nghiemvandao@gmail.com>
2018-11-26 01:53:34 +0700

    Extract current_user object

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,20 +2,14 @@ class ApplicationController < ActionController::Base
   protected
 
   def authenticate_user!
-    redirect_to new_session_path unless signed_in?
-  end
-
-  def current_email
-    session[:current_email]
+    redirect_to new_session_path unless current_user.signed_in?
   end
 
   def sign_in_as(email)
     session[:current_email] = email
   end
 
-  private
-
-  def signed_in?
-    current_email.present?
+  def current_user
+    User.new(session[:current_email])
   end
 end
