67cd6bade979415e0867f9d1e6c398c8f4feb57a
nghiemdao <nghiemvandao@gmail.com>
2018-11-25 20:04:11 +0700

    Add smoke test for homepage

---
+++ spec/features/user_visists_homepage_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+RSpec.feature 'User visits homepage', type: :feature do
+  scenario 'successfully' do
+    visit root_path
+
+    expect(page).to have_css('h1', text: 'Todos')
+  end
+end
