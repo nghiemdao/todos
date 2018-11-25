9c491fecf9c656b03cc4bb3c641db63662b7996f
nghiemdao <nghiemvandao@gmail.com>
2018-11-26 03:23:34 +0700

    Refactor session path

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
     resource :completion, only: %i[create destroy]
   end
 
-  resources :session, only: %i[new create]
+  resource :session, only: %i[new create]
 end
